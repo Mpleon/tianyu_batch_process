a9f70f7ad961d51ff2356d2cd18615e5bb8ebc0f
Mpleon <94671694+Mpleon@users.noreply.github.com>
2025-09-19 17:59:43 +0800

    批量抽奖请求

---
+++ batchRequest.py
@@ -0,0 +1,33 @@
+import requests
+import time
+
+def main():
+
+    cookie = input("请输入cookie").strip()
+    cookies = []
+    while input().strip() != "y":
+        cookie = input("请输入cookie").strip()
+        cookies.append(cookie)
+    times = int(input("请输入抽奖次数"))
+
+    headers = {
+        'Cookie':cookie,
+        'Content-Type': 'application/json'
+    }
+
+    cnt = 0
+    for
+    while cnt < times:
+        time.sleep(1)
+        cnt += 1
+        response = requests.Session().post(
+            "https://ccc.hi.163.com/ty-ty_xz_20250901_server/ccc-lottery/get/luck",
+            headers=headers,
+            timeout=30
+        )
+        print("第" + str(cnt) + "次请求, 请求结果:" + response.text)
+
+
+if __name__ == "__main__":
+    main()
+
